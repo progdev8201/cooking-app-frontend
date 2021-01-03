7e9f487e122a2ec195501427f2d21362a804cf21
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2021-01-02 23:35:55 -0500

    Update jenkins-home-server.sh

--- jenkins-home-server.sh
+++ jenkins-home-server.sh
@@ -8,23 +8,23 @@ pipeline {
                 credentialsId: 'git-credentials',
                 url: 'https://github.com/progdev8201/cooking-app-frontend.git'
                 
-                sh("""
-                    docker-compose -f docker-compose-home-server.yml down;
-                    docker system prune -a -f;
-                    docker login --username orewaprogdev13 --password kulotte2015;
-                    docker build --build-arg environement=dev -t orewaprogdev13/cooking-app .;
-                    docker push orewaprogdev13/cooking-app;
-                """)
+                sh 'docker-compose -f docker-compose-home-server.yml down'
+
+                sh 'docker system prune -a -f'
                 
+                sh 'docker login --username orewaprogdev13 --password kulotte2015'
+                
+                sh 'docker build --build-arg environement=dev -t orewaprogdev13/cooking-app .'
+                
+                sh 'docker push orewaprogdev13/cooking-app'
             }
         }
         
          stage('deploy app CD') {
             steps {
-                sh("""
-                    docker system prune -a -f;
-                    docker-compose -f docker-compose-home-server.yml up -d;
-                """)
+                sh 'docker system prune -a -f'
+
+                sh 'docker-compose -f docker-compose-home-server.yml up -d'
            }
         }
     }
